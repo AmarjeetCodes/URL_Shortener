6940fe2169a346549a5759a3686d14395087fee2
Amarjeet Anand <amarjeetanand0987@gmail.com>
2025-04-13 02:34:01 +0530

    again basse URL something

--- server/index.js
+++ server/index.js
@@ -6,46 +6,36 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const urlModel = require("./urlModel");
 
-
-
 const mongoURI = process.env.MONGO_URI;
 
-
 mongoose.connect(mongoURI)
-  .then(() => console.log(" Connected to MongoDB Atlas"))
-  .catch((err) => console.error(" MongoDB connection error:", err));
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://url-shortener-frontend-6vq0.onrender.com'],
+    origin: 'https://url-shortener-frontend-6vq0.onrender.com',
     credentials: true
-  }));
+}));
 app.use(bodyParser.json());
 
 app.post("/shorten", async (req, res) => {
-
-  
-
     const { longURL } = req.body;
     console.log("Received longURL:", longURL);
 
     const shortCode = Math.random().toString(36).substring(2, 7);
-    const shortURL = `http://localhost:5000/${shortCode}`;
+    const shortURL = `${process.env.BASE_URL}/${shortCode}`;
     console.log("Generated shortURL:", shortURL);
 
-    
-
     try {
-
-
-        const newURL = new urlModel({ longURL, shortURL:shortCode });
+        const newURL = new urlModel({ longURL, shortURL: shortCode });
         console.log("Saving to MongoDB:", newURL);
         await newURL.save();
 
         console.log("Saved successfully:", newURL);
-        res.json({ shortURL: `http://localhost:5000/${shortCode}` });
+        res.json({ shortURL: shortURL });
     } catch (err) {
         console.error("Error while saving:", err);
         res.status(500).json({ error: "Internal Server Error", details: err.message });
@@ -73,8 +63,6 @@ app.get("/:shortCode", async (req, res) => {
     }
 });
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at ${process.env.BASE_URL || `http://localhost:${PORT}`}`);
 });
